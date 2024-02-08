43f3973c22e71f7707128656738b1f85bd2e1202
jeconiahbelvaa <97310693+jeconiahb@users.noreply.github.com>
2024-02-08 21:41:26 +0700

    Update testToggle.js
    add step to color tab

--- test/android/testToggle.js
+++ test/android/testToggle.js
@@ -2,6 +2,19 @@ export const testToggle = async (driver) => {
   try {
     console.log("Test 2 Running");
     driver.activateApp("com.example.material_3_demo");
+
+    const colorTab = await driver.$(
+      '//android.view.View[@content-desc="Color\nTab 2 of 4"]'
+    );
+    console.log("Open color Tab");
+    await colorTab.click();
+
+    const colorContent = await driver.$(
+      '//android.view.View[contains(content-desc, "Light ColorScheme")]'
+    );
+    await colorContent.isDisplayed;
+    await driver.pause(2000);
+
     const toggleTheme = await driver.$(
       '//android.view.View[1]/android.view.View[2]/android.widget.Button'
     );
